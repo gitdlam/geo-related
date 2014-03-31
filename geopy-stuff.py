fc7990be7aaec2b7e7014d946cd7bf81205ee537
gitdlam <gitdlam@users.noreply.github.com>
2014-03-31 17:15:46 +1100

    Update geopy-stuff.py

--- geopy-stuff.py
+++ geopy-stuff.py
@@ -4,3 +4,9 @@ g = geocoders.GoogleV3()
 
 place, (lat, lng) = g.geocode("10900 Euclid Ave in Cleveland")
 
+
+from geopy import distance
+
+_, ne = g.geocode('Newport, RI')  
+_, cl = g.geocode('Cleveland, OH')  
+distance.distance(ne, cl).km
